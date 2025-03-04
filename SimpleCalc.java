947a3329a29442fd029b43782de92aee251e4e9e
AnanyaK07 <ananyak0708@gmail.com>
2025-03-03 19:31:12 -0800

    Add files via upload

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -1,4 +1,4 @@
-import java.util.List; // used by expression evaluator
+import java.util.List;
 import java.util.ArrayList;
 
 /**
@@ -15,16 +15,16 @@ public class SimpleCalc {
     private ExprUtils utils; // expression utilities
     private ArrayStack<Double> valueStack;	// expression utilities
     private ArrayStack<String> operatorStack;	// operator stack
-    private List<Variable> variables;
+    private List<Identifier> variables;
 
 	// constructor
     public SimpleCalc() {
         utils = new ExprUtils();
         valueStack = new ArrayStack<>();
         operatorStack = new ArrayStack<>();
-        variables = new ArrayList<Variable>();
-        variables.add(e, Math.E);
-        variables.add(pi, Math.PI);
+        variables = new ArrayList<Identifier>();
+        variables.add(new Identifier("e", Math.E));
+        variables.add(new Identifier("pi", Math.PI));
     }
 
     public static void main(String[] args) {
@@ -49,10 +49,16 @@ public class SimpleCalc {
                 printHelp();
             else if (userInput.equalsIgnoreCase("Q"))
                 continueOn = false;
+            else if (userInput.equalsIgnoreCase("L"))
+                listVariables();
             else {
                 List<String> tokens = utils.tokenizeExpression(userInput);
-                double value = evaluateExpression(tokens);
-                System.out.println(value);
+                if (tokens.size() > 2 && tokens.get(1).equals("=")) 
+                    assignVariable(tokens);
+                else {
+                    double value = evaluateExpression(tokens);
+                    System.out.println(value);
+                }
             }
         }
     }
@@ -67,14 +73,47 @@ public class SimpleCalc {
         System.out.println("  parentheses '(' and ')'\n");
     }
 
+    /**  List all identifiers/variables */
+    public void listVariables() {
+        System.out.println("Identifiers:");
+        for (Identifier id : variables) {
+            System.out.println("  " + id.getName() + " = " + id.getValue());
+        }
+    }
+
+    /** Assign a variable with an expression
+     */
+    public void assignVariable(List<String> tokens) {
+        String varName = tokens.get(0);
+        List<String> exprTokens = tokens.subList(2, tokens.size());
+        double value = evaluateExpression(exprTokens);
+        
+        // Check if variable already exists
+        Identifier var = findVariable(varName);
+        if (var == null) {
+            variables.add(new Identifier(varName, value));
+        } else {
+            var.setValue(value);
+        }
+        System.out.println( "  " + varName + " = " + value);
+    }
+
+    // Find variable by name
+    private Identifier findVariable(String name) {
+        for (Identifier id : variables) {
+            if (id.getName().equals(name)) {
+                return id;
+            }
+        }
+        return null;
+    }
+
 	/**
      *    Evaluate expression and return the value
      *    @param tokens    a List of String tokens making up an arithmetic expression
      *    @return            a double value of the evaluated expression
      */
     public double evaluateExpression(List<String> tokens) {
-		if(tokens.get(1).equals("="))
-			String varName = tokens.get(0);
         for (int i = 0; i < tokens.size(); i++) {
             String token = tokens.get(i);
             char c = token.charAt(0);
@@ -85,45 +124,52 @@ public class SimpleCalc {
                 operatorStack.push(token);
             else if (token.equals(")")) {
                 while (!operatorStack.isEmpty() && !operatorStack.peek().equals("(")) {
-					String op = operatorStack.pop();
-					double b = valueStack.pop();
-					double a = valueStack.pop();
-					if (op.equals("+")) valueStack.push(a + b);
-					else if (op.equals("-")) valueStack.push(a - b);
-					else if (op.equals("*")) valueStack.push(a * b);
-					else if (op.equals("/")) valueStack.push(a / b);
-					else if (op.equals("%")) valueStack.push(a % b);
-					else if (op.equals("^")) valueStack.push(Math.pow(a, b));
+                    String op = operatorStack.pop();
+                    double b = valueStack.pop();
+                    double a = valueStack.pop();
+                    if (op.equals("+")) valueStack.push(a + b);
+                    else if (op.equals("-")) valueStack.push(a - b);
+                    else if (op.equals("*")) valueStack.push(a * b);
+                    else if (op.equals("/")) valueStack.push(a / b);
+                    else if (op.equals("%")) valueStack.push(a % b);
+                    else if (op.equals("^")) valueStack.push(Math.pow(a, b));
                 }
                 operatorStack.pop(); 
             } 
             else if (utils.isOperator(c)) {
                 while (!operatorStack.isEmpty() && hasPrecedence(token, operatorStack.peek())) {
-					String op = operatorStack.pop();
-					double b = valueStack.pop();
-					double a = valueStack.pop();
-					if (op.equals("+")) valueStack.push(a + b);
-					else if (op.equals("-")) valueStack.push(a - b);
-					else if (op.equals("*")) valueStack.push(a * b);
-					else if (op.equals("/")) valueStack.push(a / b);
-					else if (op.equals("%")) valueStack.push(a % b);
-					else if (op.equals("^")) valueStack.push(Math.pow(a, b));
+                    String op = operatorStack.pop();
+                    double b = valueStack.pop();
+                    double a = valueStack.pop();
+                    if (op.equals("+")) valueStack.push(a + b);
+                    else if (op.equals("-")) valueStack.push(a - b);
+                    else if (op.equals("*")) valueStack.push(a * b);
+                    else if (op.equals("/")) valueStack.push(a / b);
+                    else if (op.equals("%")) valueStack.push(a % b);
+                    else if (op.equals("^")) valueStack.push(Math.pow(a, b));
                 }
                 operatorStack.push(token);
+            } else {
+                Identifier var = findVariable(token);
+                if (var != null) 
+                    valueStack.push(var.getValue());
+                else 
+                    valueStack.push(0.0);  
             }
         }
+
         while (!operatorStack.isEmpty()) {
-			String op = operatorStack.pop();
-			double b = valueStack.pop();
-			double a = valueStack.pop();
-			if (op.equals("+")) valueStack.push(a + b);
+            String op = operatorStack.pop();
+            double b = valueStack.pop();
+            double a = valueStack.pop();
+            if (op.equals("+")) valueStack.push(a + b);
             else if (op.equals("-")) valueStack.push(a - b);
-        	else if (op.equals("*")) valueStack.push(a * b);
+            else if (op.equals("*")) valueStack.push(a * b);
             else if (op.equals("/")) valueStack.push(a / b);
             else if (op.equals("%")) valueStack.push(a % b);
             else if (op.equals("^")) valueStack.push(Math.pow(a, b));
-		}
-		
+        }
+        
         return valueStack.pop();
     }
 
@@ -142,9 +188,8 @@ public class SimpleCalc {
     private boolean hasPrecedence(String op1, String op2) {
         if (op2.equals("(") || op2.equals(")")) return false;
         if (op1.equals("^") && !op2.equals("^")) return false;
-        if ((op1.equals("*") || op1.equals("/") ||
-			 op1.equals("%")) && (op2.equals("+") || 
-			 op2.equals("-"))) return false;
+        if ((op1.equals("*") || op1.equals("/") || op1.equals("%")) && 
+            (op2.equals("+") || op2.equals("-"))) return false;
         return true;
     }
 }
